d39cfe324e481c755d0cfb98d74c3b687ffecbf0
Valur Einarsson <valure@live.com>
2025-05-02 12:22:15 +0100

    Add apphost for running minarsidur.

---
+++ apphost/AppHost/BuilderExtensions.cs
@@ -0,0 +1,48 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppHost
+{
+  internal static class BuilderExtensions
+  {
+    public static IResourceBuilder<NodeAppResource> AddNodeService(this IDistributedApplicationBuilder builder, string app, Dictionary<string, string> secrets)
+    {
+      var resource = new NodeAppResource(app, "yarn", ".");
+
+      string healthBaseUrl;
+      switch (app)
+      {
+        case "services-user-profile":
+          healthBaseUrl = "http://localhost:3366";
+          break;
+        case "services-bff":
+          healthBaseUrl = "http://localhost:3010/bff";
+          break;
+        case "api":
+          healthBaseUrl = "http://localhost:4444";
+          break;
+        default:
+          throw new ArgumentException($"Unknown service: {app}");
+      }
+
+      builder.Services.AddHealthChecks().AddUrlGroup(new Uri($"{healthBaseUrl}/health/check"), name: app);
+
+      var service = builder.AddResource(resource).WithArgs(["start", app])
+          .WithEnvironment("NODE_TLS_REJECT_UNAUTHORIZED", "0")
+          .WithHealthCheck(app);
+
+      foreach (var secret in secrets)
+      {
+        service.WithEnvironment(secret.Key, secret.Value);
+      }
+
+      return service;
+    }
+
+    public static IResourceBuilder<NodeAppResource> AddDatabase(this IDistributedApplicationBuilder builder, string app)
+    {
+      var resource = new NodeAppResource($"start-dev-{app}", "yarn", ".");
+
+      return builder.AddResource(resource).WithArgs(["dev-services", app]);
+    }
+  }
+}
---
+++ apphost/README.md
@@ -0,0 +1,13 @@
+Start AppHost in Visual Studio
+
+```
+F5
+```
+
+In terminal
+
+```
+dotnet run --project .\apphost\AppHost --launch-profile https
+```
+
+Go to http://localhost:4200/minarsidur.
